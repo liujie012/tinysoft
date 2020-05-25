8cb69f16697f0c8ed5a3ac123662fe10020bc5c0
liujie <liujie012@aliyun.com>
2020-05-25 18:19:47 +0800

    增加qq列表

---
+++ app/controller/Qq.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace app\controller;
+
+use think\facade\View;
+
+
+class Qq
+{
+
+    public function seven()
+    {
+        return View::fetch();
+    }
+
+    public function eight()
+    {
+        return View::fetch();
+    }
+
+    public function nine()
+    {
+        return View::fetch();
+    }
+}
